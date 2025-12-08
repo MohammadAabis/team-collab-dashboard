054781dd9f297241542fabb04c12ea251e5eba51
Mohammad Aabis <101064297+MohammadAabis@users.noreply.github.com>
2025-12-08 22:42:34 +0500

    jwt implementation for frontend

---
+++ client/src/types/auth.ts
@@ -0,0 +1,9 @@
+export interface RegisterFormData {
+  first_name: string;
+  last_name: string;
+  email: string;
+  address: string;
+  phone: string;
+  password: string;
+  confirmPassword: string;
+};
