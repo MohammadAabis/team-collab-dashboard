a036c1ab301fe8f6dd8cf0255fe956ad6be53443
Mohammad Aabis <101064297+MohammadAabis@users.noreply.github.com>
2026-01-08 22:36:53 +0500

    server routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -9,3 +9,18 @@ router.post("/login", login)
 // router.get("/profile", authMiddleware, getProfile);
 
 export default router;
+
+
+// import express from "express";
+
+// import { register, login, logout, refreshToken } from "../controllers/auth.controller";
+
+// const router = express.Router();
+
+
+// router.post("/register", register);
+// router.post("/login", login);
+// router.post("/logout", logout)
+// router.get("/refresh", refreshToken);
+
+// export default router;
