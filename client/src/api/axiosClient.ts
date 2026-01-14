9a61620eb4e8167cb653c02bdf1047bd51c027ec
Mohammad Aabis <101064297+MohammadAabis@users.noreply.github.com>
2026-01-14 23:44:29 +0500

    auto refresh token

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -4,10 +4,6 @@ import axios from "axios";
 import { getAccessToken, setAccessToken } from "../utils/tokenMemory";
 import { api as axiosClient } from "./auth.api";
 
-// const axiosClient = axios.create({
-//   baseURL: "http://localhost:5000",
-//   withCredentials: true,  // important for cookies
-// });
 
 // Attach access token on each request
 axiosClient.interceptors.request.use((config) => {
@@ -46,90 +42,3 @@ axiosClient.interceptors.response.use(
 );
 
 export default axiosClient;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from "axios";
-// import { useAuth } from "../context/AuthContext";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5000",
-//   withCredentials: true, // IMPORTANT for refresh token cookie
-// });
-
-// // Create a function hook to attach interceptors inside React
-// export const setupAxiosInterceptors = (auth: ReturnType<typeof useAuth>) => {
-//   // Request: attach token
-//   api.interceptors.request.use((config) => {
-//     if (auth.accessToken) {
-//       config.headers.Authorization = `Bearer ${auth.accessToken}`;
-//     }
-//     return config;
-//   });
-
-//   // Response: auto refresh token
-//   api.interceptors.response.use(
-//     (res) => res,
-//     async (err) => {
-//       const originalRequest = err.config;
-
-//       // If access token expired
-//       if (err.response?.status === 401 && !originalRequest._retry) {
-//         originalRequest._retry = true;
-
-//         try {
-//           const refreshResponse = await api.get("/auth/refresh");
-
-//           const newAccessToken = refreshResponse.data.accessToken;
-
-//           auth.login(newAccessToken, auth.user!); // update context token
-
-//           originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-//           return api(originalRequest); // retry
-//         } catch {
-//           auth.logout();
-//           return Promise.reject(err);
-//         }
-//       }
-
-//       return Promise.reject(err);
-//     }
-//   );
-// };
-
-// export default api;
