51b9154a30d1d7549cc9ff00b471ea19d368f87b
Mohammad Aabis <101064297+MohammadAabis@users.noreply.github.com>
2026-05-31 23:11:08 +0500

    Update generateToken.ts

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -1,13 +1,18 @@
 import jwt from "jsonwebtoken";
 
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET as string;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
+
 export const generateAccessToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_ACCESS_SECRET as string, {
+  return jwt.sign({ id }, JWT_ACCESS_SECRET, {
+    algorithm: "HS256",
     expiresIn: "15m",
   });
 };
 
 export const generateRefreshToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET as string, {
+  return jwt.sign({ id }, JWT_REFRESH_SECRET, {
+    algorithm: "HS256",
     expiresIn: "7d",
   });
 };
